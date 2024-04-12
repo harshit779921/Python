af0f710cb1ed81a40d450fe53a46513985510c84
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2024-04-12 13:51:55 +0530

    just formatting

--- Python/p18.py
+++ Python/p18.py
@@ -1,9 +1,11 @@
-#wap to find the factorial of n(n is the parameter)
+# wap to find the factorial of n(n is the parameter)
+
 
 def cal_fact(n):
-     fact=1
-     for i in range (1,n+1):
-          fact *=i
-     print(fact)    
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    print(fact)
 
-cal_fact(5)       
+
+cal_fact(5)
