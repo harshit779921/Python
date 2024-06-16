154b1e04e24e72deb4b9d39d4c5aa8bb3d8e7b0b
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2024-06-17 00:56:04 +0530

    comment which help in making the code for encode to decode

--- Python/Python Program for interview/p7.py
+++ Python/Python Program for interview/p7.py
@@ -6,3 +6,5 @@ decode = " "
 for i in range(len(encode)):
     decode += alphabets[(alphabets.index(encode[i]) - 3) % 26]
 print("Message is :",decode)
+
+# if ypu want to make it the decode then its simple jus add +3
