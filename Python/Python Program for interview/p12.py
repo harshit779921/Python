4422972a0651e4d75fad29afdd27caa9cd47fed4
Harshit Pandey <108739074+harshit779921@users.noreply.github.com>
2024-06-21 22:48:34 +0530

    Find int values from a file

---
+++ Python/Python Program for interview/p12.py
@@ -0,0 +1,12 @@
+# Find int values from a file
+
+total = 0
+
+with open('Python\Python Program for interview\p12_sample.txt', "r") as file:
+    for line in file:
+        words = line.split()
+        for word in words:
+            if word.isdigit():
+                total = total + int(word)
+
+print("The total of int in file:", total)
