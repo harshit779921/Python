2d2fa71430f5bbc7f3d1e971c2c7809d60706507
harshit779921 <pusphadevipandey8@gmail.com>
2024-08-21 20:56:56 +0530

    Statistical operations on 1D Array

---
+++ Python Program for interview/p15.py
@@ -0,0 +1,26 @@
+# Statistical operations on 1D Array
+
+import numpy as np
+
+x = np.array([4, 2, 3, 9, 7 ])
+
+a = np.max(x)
+print(a)
+
+b = np.min(x)
+print(b)
+
+c = np.mean(x)
+print(c)
+
+d = np.median(x)
+print(d)
+
+f = np.var(x)
+print(f)
+
+g = np.std(x)
+print(g)
+
+e = np.prod(x)
+print(e)
